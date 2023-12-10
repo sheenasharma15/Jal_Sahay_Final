cbc8213d09e6bb99eb00333c5dbda37fe3f08549
Sheena Sharma <sharmasheena1973@gmail.com>
2023-12-10 15:09:07 +0530

    JAL SAHAY APP

---
+++ app/src/main/java/com/internshala/jalsahayfinal/FeedbackForm.kt
@@ -0,0 +1,11 @@
+package com.internshala.jalsahayfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeedbackForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feedback_form)
+    }
+}
