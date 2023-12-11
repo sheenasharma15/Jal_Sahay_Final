0f0ea6228850477be8cf27f017ad780fa20d923b
Sheena Sharma <sharmasheena1973@gmail.com>
2023-12-11 12:52:16 +0530

    JAL SAHAY APP

--- app/src/main/java/com/internshala/jalsahayfinal/MainActivity.kt
+++ app/src/main/java/com/internshala/jalsahayfinal/MainActivity.kt
@@ -1,22 +1,30 @@
 package com.internshala.jalsahayfinal
 
-import android.annotation.SuppressLint
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val userLoginButton: View = findViewById(R.id.btnUserLogin)
+        val userLoginButton: Button = findViewById(R.id.btnUserLogin)
         userLoginButton.setOnClickListener {
+
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+       val employeeLoginButton: Button = findViewById(R.id.btnEmployeeLogin)
+        employeeLoginButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity:: class.java)
+            startActivity(intent)
+        }
+
     }
 }
+
