cce53f745d6d31dca1789b64e42e19e6dce70ba9
Priya Chauhan <priyachauhan.kkr@gmail.com>
2023-12-12 12:51:30 +0530

    Changes made

--- app/src/main/java/com/internshala/jalsahayfinal/Activity/SplashScreen.kt
+++ app/src/main/java/com/internshala/jalsahayfinal/Activity/SplashScreen.kt
@@ -13,7 +13,7 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this,UserEmployeeActivity::class.java))
             finish()
         },3000)
     }
