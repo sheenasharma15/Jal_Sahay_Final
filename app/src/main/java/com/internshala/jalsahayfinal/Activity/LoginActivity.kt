f79ed160976bba34895b5ff7d0f20a9d1f7964ae
Priya Chauhan <priyachauhan.kkr@gmail.com>
2023-12-12 14:26:25 +0530

    Changes made

--- app/src/main/java/com/internshala/jalsahayfinal/Activity/LoginActivity.kt
+++ app/src/main/java/com/internshala/jalsahayfinal/Activity/LoginActivity.kt
@@ -1,13 +1,11 @@
 package com.internshala.jalsahayfinal.Activity
 
-
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import com.internshala.jalsahayfinal.R
-
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
